3beb2c80563c8e76f5bf0f1347a475c99209066a
Anna Si Tu <asitu03@student.ubc.ca>
2026-02-02 21:16:33 -0800

    Crreated dashboard

--- dataPilot.py
+++ dataPilot.py
@@ -1,5 +1,10 @@
-# import requests
+import requests
+import pandas as pd
+import os
 
+# install packages: python -m pip install requests
+
+# CODE FOR GETTING INFORMATION FROM OuraAPI
 # TOKEN_URL = "https://api.ouraring.com/oauth/token"
 
 # data = {
@@ -14,12 +19,7 @@
 # response.raise_for_status()
 
 # tokens = response.json()
-# print(tokens)
-# Use printed tokens for access_token
-
-import requests
-import pandas as pd
-# import os
+# print(tokens)                         # Use printed tokens for access_token
 
 ACCESS_TOKEN = "ACCESS_TOKEN_HERE"
 
@@ -27,31 +27,31 @@ headers = {
     "Authorization": f"Bearer {ACCESS_TOKEN}"
 }
 
+# url changes depending on what you want to
 url = "https://api.ouraring.com/v2/usercollection/heartrate"
 
-# heartrate during day + sleep, HRV, steps
+# collect: heartrate during day + sleep, HRV, steps
 
+# Ensure params are large enough to capture all day
 params = {
     "start_datetime": "2026-01-20T00:00:00",
     "end_datetime": "2026-01-28T23:59:59"
 }
 
-# Ensure params are large enough to capture all day
-
 r = requests.get(url, headers = headers, params = params)
 r.raise_for_status()
 
 data = r.json()["data"]
-df = pd.DataFrame(data)
+df = pd.DataFrame(data)                 # Convert data into df
 
-# print(df.head())      # View data
+# print(df.head())                      # View data
 
 df["timestamp"] = pd.to_datetime(df["timestamp"])
 
+# Convert df to csv to be saved locally
 df.to_csv(
     "oura_heartrate_pilot.csv",
     index = False
 )
 
-# Find where the file went
-# print("Saving to:", os.getcwd())
+# print("Saving to:", os.getcwd())      # Find where the file went
